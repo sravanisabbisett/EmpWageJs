222defd2c6071bbd7670ca043ed529d3ac71244f
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-21 15:20:02 +0530

    ADDED:Function to get employee hours

--- employee.js
+++ employee.js
@@ -1,33 +1,21 @@
-//uc1 check employee
-{
-    const IS_ABSENT=0;
-    let empCheck=Math.floor(Math.random()*10)%2;
-    if(empCheck==IS_ABSENT){
-        console.log("UC1_Employee is absent");
-    }else{
-        console.log("UC1_Employee is present")
+//uc3 function for getting working hours
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HRS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HRS;
+            break;
+        default:
+            return 0;
     }
 }
-
-//uc2 calculate employee wage
-const IS_PART_TIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HOURS=4;
-const FULL_TIME_HOURS=8;
-const WAGE_PER_HOUR=20;
-{
-let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
-switch(empCheck){
-    case IS_FULL_TIME:
-        empHrs=FULL_TIME_HOURS;
-        break;
-    case IS_PART_TIME:
-        empHrs=PART_TIME_HOURS;
-        break;
-    default:
-        empHrs=0;
-}
-let empWage=empHrs*WAGE_PER_HOUR;
-console.log("UC2_Emp Wage:"+empWage);
-}
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empWage = getWorkingHours(empCheck) * WAGE_PER_HR;
+console.log("Emp wage:" + empWage);
