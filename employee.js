805d6d5543ecb94a354a45b95b5bbb26432b93d6
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-21 15:01:41 +0530

    ADDED:Random function to check employee is persent or absent

--- employee.js
+++ employee.js
@@ -1,1 +1,10 @@
-console.log("Welcome to employeewage");
+//uc1 check employee
+{
+    const IS_ABSENT=0;
+    let empCheck=Math.floor(Math.random()*10)%2;
+    if(empCheck==IS_ABSENT){
+        console.log("UC1_Employee is absent");
+    }else{
+        console.log("UC1_Employee is present")
+    }
+}
