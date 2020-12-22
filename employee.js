d137e7b823e070cf5f9995ab7dfe4453f573372b
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-22 16:57:38 +0530

    ADDED:Object arrow operations

--- employee.js
+++ employee.js
@@ -46,4 +46,28 @@ while (totalEmpHrs <= MAX_WORK_HRS && totalWorkingDays < Number_Of_Working_Days)
         });
 }
 console.log("UC10 store daily wage, work, hours day in a object: " + empDailyHrsAndWageArr);
-  
+console.log("UC10 store daily wage, work, hours day in a object: " + empDailyHrsAndWageArr);
+//UC 11.a Calculate total hours and wage using arrow functions
+let totalHours = empDailyHrsAndWageArr
+    .filter(obj => obj.dailyHours > 0)
+    .reduce((totalHours, obj) => totalHours += obj.dailyHours, 0);
+let totalWage = empDailyHrsAndWageArr
+    .filter(obj => obj.dailyWage > 0)
+    .reduce((totalWage, obj) => totalWage += obj.dailyWage, 0);
+console.log("\nUC 11.a Calculate total hours and wage using arrow functions");
+console.log("Total Hours: " + totalHours + "   Total Wage: " + totalWage);
+//UC 11.b Show the full working days using foreach
+console.log("\nUC 11.b Show the full working days");
+empDailyHrsAndWageArr.filter(obj => obj.dailyHours == 8).forEach(obj => process.stdout.write(obj.toString()));
+//UC 11.c Show Part working days using Map by reducing to String Array
+let partWorkingDays = empDailyHrsAndWageArr
+    .filter(obj => obj.dailyHours == 4)
+    .map(obj => obj.toString());
+console.log("\nUC 11.c Show part time working days");
+console.log(partWorkingDays);
+//UC 11.d Show No working days using Map by reducing to String Array
+let noWorkingDays = empDailyHrsAndWageArr
+    .filter(obj => obj.dailyHours == 0)
+    .map(obj => obj.toString());
+console.log("\nUC 11.d Show no working days");
+console.log(noWorkingDays);
