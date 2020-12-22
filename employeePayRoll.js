4f77392fcc6ca08f7b76392acf8027f72703d9d0
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-22 17:24:02 +0530

    ADDED:Regex to validte the name

--- employeePayRoll.js
+++ employeePayRoll.js
@@ -3,21 +3,34 @@ class employeePayRollData{
     id;
     salary;
 
-    constructor(id,name,salary){
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+    // constructor
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
     }
     get name(){return this._name};
-    set name(name){this._name=name};
+    set name(name){
+        let nameRegex=RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if(nameRegex.test(name)) this._name=name;
+        else throw 'Name is in incorrectFormat';
+    };
 
     toString(){
-        return "ID:"+this.id+", name:"+this.name+",salary:"+this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + empDate;0
     }
 }
 
     let EmployeePayRollData=new employeePayRollData(1,"Mark",30000);
     console.log(EmployeePayRollData.toString());
+    try{
     EmployeePayRollData.name="Sravani";
     console.log(EmployeePayRollData.toString());
+    }catch(e){
+        console.error(e);
+    }
     
